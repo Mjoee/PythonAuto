c112bc9088a4c6c7e1c1410280333a58b66fea78
Nikita Piatiletov <pyatiletovnick@gmail.com>
2023-02-13 20:24:02 +0200

    start

---
+++ lesson_1.py
@@ -0,0 +1,18 @@
+def multiply_by_two(x):
+    return x*2
+
+
+class MyFirstClass:
+    def first_method(self):
+        pass
+
+    def second_method(self):
+        pass
+
+
+class MySecondClass:
+    pass
+
+
+def demo_function():
+    pass
