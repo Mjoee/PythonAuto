215067f10f248c61f55cd21a1a6b67ffe228754e
Nikita Piatiletov <pyatiletovnick@gmail.com>
2023-02-13 20:38:15 +0200

    test

--- lesson_1.py
+++ lesson_1.py
@@ -16,3 +16,15 @@ class MySecondClass:
 
 def demo_function():
     pass
+
+first_variable = 5
+second_variable = 10
+third_variable = 2
+result = (first_variable +
+          second_variable
+          - third_variable
+          )
+
+x = 3
+if x > 5:
+    print('x is larger that 5')
