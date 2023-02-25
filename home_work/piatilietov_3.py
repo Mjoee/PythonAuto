b25d5dba9ba31814b0c7f7c70ecfdfd7c807e37a
Nikita <pyatiletovnick@gmail.com>
2023-02-25 18:39:54 +0200

    HW3

--- home_work/piatilietov_3.py
+++ home_work/piatilietov_3.py
@@ -1,31 +1,34 @@
+# создание списка
 notes = []
-
+# создание цикла
 while True:
+    # ввод экшена
     action = input("Enter action (add/earliest/latest/longest/shortest/Exit): ")
 
+    # добавление элемента в список
     if action == "add":
         note = input("Enter note: ")
         notes.append(note)
         print("Note added.")
-
+    # сортировка от старой к новой
     elif action == "earliest":
         sorted_notes = sorted(notes)
         print("Notes (earliest to latest):")
         for note in sorted_notes:
             print(note)
-
+    # сортировка от новой к старой
     elif action == "latest":
         sorted_notes = sorted(notes, reverse=True)
         print("Notes (latest to earliest):")
         for note in sorted_notes:
             print(note)
-
+    # сортировка с длинного по короткий
     elif action == "longest":
         sorted_notes = sorted(notes, key=len, reverse=True)
         print("Notes (longest to shortest):")
         for note in sorted_notes:
             print(note)
-
+    # сортировка с короткого по длинный
     elif action == "shortest":
         sorted_notes = sorted(notes, key=len)
         print("Notes (shortest to longest):")
@@ -35,6 +38,6 @@ while True:
     elif action == "Exit":
         print("Exiting program.")
         break
-
+    # если вводится что-то другое падаем в елсе и идем по кругу
     else:
         print("Invalid action. Please try again.")
