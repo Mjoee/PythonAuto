bc7b7c3682653c61a50c23af47ff06c5d2487ccd
Nikita <pyatiletovnick@gmail.com>
2023-02-25 18:34:58 +0200

    HW3

--- home_work/piatilietov_3.py
+++ home_work/piatilietov_3.py
@@ -1,1 +1,40 @@
 notes = []
+
+while True:
+    action = input("Enter action (add/earliest/latest/longest/shortest/Exit): ")
+
+    if action == "add":
+        note = input("Enter note: ")
+        notes.append(note)
+        print("Note added.")
+
+    elif action == "earliest":
+        sorted_notes = sorted(notes)
+        print("Notes (earliest to latest):")
+        for note in sorted_notes:
+            print(note)
+
+    elif action == "latest":
+        sorted_notes = sorted(notes, reverse=True)
+        print("Notes (latest to earliest):")
+        for note in sorted_notes:
+            print(note)
+
+    elif action == "longest":
+        sorted_notes = sorted(notes, key=len, reverse=True)
+        print("Notes (longest to shortest):")
+        for note in sorted_notes:
+            print(note)
+
+    elif action == "shortest":
+        sorted_notes = sorted(notes, key=len)
+        print("Notes (shortest to longest):")
+        for note in sorted_notes:
+            print(note)
+
+    elif action == "Exit":
+        print("Exiting program.")
+        break
+
+    else:
+        print("Invalid action. Please try again.")
